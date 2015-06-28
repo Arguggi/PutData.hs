9150aa2f244491ad062dcee20d79c6c71566020e
Arguggi <git@arguggi.co.uk>
2015-06-28 10:08:01 +0200

    Move api functions out of where

--- src/Main.hs
+++ src/Main.hs
@@ -6,15 +6,14 @@
 module Main where
 
 import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Either
 import           Data.Aeson
 import           Data.Maybe
-import           Data.Text                  as T
+import           Data.Text                as T
 import           Database.HDBC
 import           Database.HDBC.PostgreSQL
 import           GHC.Generics
 import           Network.Wai
-import qualified Network.Wai.Handler.Warp   as W
+import qualified Network.Wai.Handler.Warp as W
 import           Servant
 
 data Url = Url
@@ -44,34 +43,9 @@ userAPI :: Proxy UserAPI
 userAPI = Proxy
 
 server :: Server UserAPI
-server = urls
-    :<|> newurl
-    :<|> deleteUrl
-
-    where urls :: EitherT ServantErr IO [Url]
-          urls = liftIO getUrls
-
-          newurl :: NewUrl -> EitherT ServantErr IO Status
-          newurl insertUrl = do
-              conn <- liftIO $ connectPostgreSQL "dbname = putdata"
-              stmt <- liftIO $ prepare conn "INSERT INTO strings (string) VALUES (?)"
-              rows <- liftIO $ execute stmt [toSql $ newUrl insertUrl]
-              liftIO $ commit conn
-              liftIO $ disconnect conn
-              case rows of
-                  1 -> return Status { code = 0, message = "Url added successfully"}
-                  _ -> return Status { code = 1, message = "Error while adding url"}
-
-          deleteUrl :: Integer -> EitherT ServantErr IO Status
-          deleteUrl delUrl = do
-              conn <- liftIO $ connectPostgreSQL "dbname = putdata"
-              stmt <- liftIO $ prepare conn "DELETE FROM strings WHERE id = ?"
-              rows <- liftIO $ execute stmt [toSql delUrl]
-              liftIO $ commit conn
-              liftIO $ disconnect conn
-              case rows of
-                  1 -> return Status { code = 0, message = "Url deleted successfully"}
-                  _ -> return Status { code = 1, message = "Error while deleting url"}
+server = liftIO getUrls
+    :<|> liftIO . addUrl
+    :<|> liftIO . deleteUrl
 
 getUrls :: IO [Url]
 getUrls = do
@@ -79,6 +53,28 @@ getUrls = do
     r <- quickQuery' conn "SELECT * from strings ORDER BY id DESC" []
     disconnect conn
     return $ fmap convRow r
+
+addUrl :: NewUrl -> IO Status
+addUrl insertUrl = do
+    conn <- connectPostgreSQL "dbname = putdata"
+    stmt <- prepare conn "INSERT INTO strings (string) VALUES (?)"
+    rows <- execute stmt [toSql $ newUrl insertUrl]
+    commit conn
+    disconnect conn
+    case rows of
+        1 -> return Status { code = 0, message = "Url added successfully"}
+        _ -> return Status { code = 1, message = "Error while adding url"}
+
+deleteUrl :: Integer -> IO Status
+deleteUrl delUrl = do
+    conn <- connectPostgreSQL "dbname = putdata"
+    stmt <- prepare conn "DELETE FROM strings WHERE id = ?"
+    rows <- execute stmt [toSql delUrl]
+    commit conn
+    disconnect conn
+    case rows of
+        1 -> return Status { code = 0, message = "Url deleted successfully"}
+        _ -> return Status { code = 1, message = "Error while deleting url"}
 
 app :: Application
 app = serve userAPI server
